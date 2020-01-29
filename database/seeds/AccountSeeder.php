85c580bb8830466ef611827c530c20404dc2b1e3
imufiid <imufiid98@gmail.com>
2020-01-29 11:09:16 +0700

    mufiid_change variabel seeder

--- database/seeds/AccountSeeder.php
+++ database/seeds/AccountSeeder.php
@@ -19,25 +19,25 @@ class AccountSeeder extends Seeder
         $ass->updated_at = new DateTime();
         $ass->save();
 
-        $ass = new Account();
-        $ass->kode = '2112';
-        $ass->account = 'Gaji';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass1 = new Account();
+        $ass1->kode = '2112';
+        $ass1->account = 'Gaji';
+        $ass1->created_at = new DateTime();
+        $ass1->updated_at = new DateTime();
+        $ass1->save();
 
-        $ass = new Account();
-        $ass->kode = '2113';
-        $ass->account = 'Uang Saku';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass2 = new Account();
+        $ass2->kode = '2113';
+        $ass2->account = 'Uang Saku';
+        $ass2->created_at = new DateTime();
+        $ass2->updated_at = new DateTime();
+        $ass2->save();
 
-        $ass = new Account();
-        $ass->kode = '2114';
-        $ass->account = 'Lain-lain';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass3 = new Account();
+        $ass3->kode = '2114';
+        $ass3->account = 'Lain-lain';
+        $ass3->created_at = new DateTime();
+        $ass3->updated_at = new DateTime();
+        $ass3->save();
     }
 }
--- database/seeds/AssetSeeder.php
+++ database/seeds/AssetSeeder.php
@@ -19,32 +19,32 @@ class AssetSeeder extends Seeder
         $ass->updated_at = new DateTime();
         $ass->save();
 
-        $ass = new Asset();
-        $ass->kode = '1112';
-        $ass->asset = 'Bank BCA';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass1 = new Asset();
+        $ass1->kode = '1112';
+        $ass1->asset = 'Bank BCA';
+        $ass1->created_at = new DateTime();
+        $ass1->updated_at = new DateTime();
+        $ass1->save();
 
-        $ass = new Asset();
-        $ass->kode = '1113';
-        $ass->asset = 'Bank BNI';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass2 = new Asset();
+        $ass2->kode = '1113';
+        $ass2->asset = 'Bank BNI';
+        $ass2->created_at = new DateTime();
+        $ass2->updated_at = new DateTime();
+        $ass2->save();
 
-        $ass = new Asset();
-        $ass->kode = '1114';
-        $ass->asset = 'Bank DBS';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass3 = new Asset();
+        $ass3->kode = '1114';
+        $ass3->asset = 'Bank DBS';
+        $ass3->created_at = new DateTime();
+        $ass3->updated_at = new DateTime();
+        $ass3->save();
         
-        $ass = new Asset();
-        $ass->kode = '1115';
-        $ass->asset = 'Lain-lain';
-        $ass->created_at = new DateTime();
-        $ass->updated_at = new DateTime();
-        $ass->save();
+        $ass4 = new Asset();
+        $ass4->kode = '1115';
+        $ass4->asset = 'Lain-lain';
+        $ass4->created_at = new DateTime();
+        $ass4->updated_at = new DateTime();
+        $ass4->save();
     }
 }
